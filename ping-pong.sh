2ece7bdd11764fe12f392c00e5ed4fd0de3b5579
Igor Almeida <igor.p.almeida@gmail.com>
2017-07-11 16:20:54 -0300

    respecting follow and append flags. still need to implement requests with user@password pattern

--- ping-pong.sh
+++ ping-pong.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-#follow redirects?
-#ouput place
-
-now=$(date +%s)
-
-mkdir -p /var/log/ping-pong
-target="/var/log/ping-pong/sample.log"
+[[ "$2" == "--follow" || "$3" == "--follow"  ]] && follow=" -L " || follow=""
+[[ "$2" == "--append" || "$3" == "--append"  ]] && target="/var/log/ping-pong/sample.log" || target="/var/log/ping-pong/sample.$(date +%s).log"
 
 while read -r site; do
 
@@ -14,7 +9,7 @@ while read -r site; do
   then
 
     result=$(curl $site \
-      -w "@format.txt" --compressed -I --silent -o /dev/null \
+      -w "@format.txt" $follow --compressed -I --silent -o /dev/null \
       -H 'Pragma: akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no, akamai-x-get-request-id,akamai-x-get-nonces,akamai-x-get-client-ip,akamai-x-feo-trace' \
       -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36' \
       -H 'Referer: ${url}')
@@ -22,4 +17,4 @@ while read -r site; do
 		echo "$(date +%F_%T) $result" >> $target
 
   fi
-done <<< "$(echo $@ | tr " " "\n")"
+done <<< "$1"
