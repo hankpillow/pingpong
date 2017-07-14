5da5961542476d9295d6a22674adaf3cfe2e6855
Igor Almeida <igor.p.almeida@gmail.com>
2017-07-14 14:31:54 -0300

    fast searching and stopping when a register is out of desired start date urls withou \n at the end tidy code and comments logging better activities

--- api/pingpong.py
+++ api/pingpong.py
@@ -1,16 +1,14 @@
-#TODO- parar loop quando a data estiver antiga
-#TODO- array reverse
-#TODO- formato de tempo mais flexivel
 
 from datetime import datetime, timedelta
-import json
 import falcon
+import json
 import logging
+import re
 import sys
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-DEFAULT_HOST = "*"
+DEFAULT_HOST = ""
 DATE_TEMPLATE = "%Y-%m-%d_%H:%M:%S"
 
 LOGGER = logging.getLogger('[pingpong]')
@@ -29,40 +27,53 @@ def sanitize_data(req, resp, resource, params):
     """normalize params before parsing db"""
 
     try:
-        params["start_date"] = TIME.get(params.get("start_date"), "all")
+        params["start_date"] = TIME.get(params.get("start_date"), "now")
         params["host"] = req.get_param("host", default=DEFAULT_HOST)
 
     except Exception as error:
         LOGGER.error(error)
-        raise falcon.HTTPError(falcon.HTTP_400, 'cant sanitize params')
+        raise falcon.HTTPError(falcon.HTTP_400, 'unexpected path')
 
 def format_sample(arr):
     """format arr into curl sample"""
 
-    return {
-        "type": "sample",
-        "date": datetime.strptime(arr[0], DATE_TEMPLATE),
-        "http_code": arr[1],
-        "time_namelookup": arr[2],
-        "time_connect": arr[3],
-        "time_appconnect": arr[4],
-        "time_pretransfer": arr[5],
-        "time_redirect": arr[6],
-        "time_starttransfer": arr[7],
-        "time_total": arr[8],
-        "num_redirects": arr[9],
-        "url": str(arr[10]).replace("\n", "")
-    }
+    try:
+        return {
+            "type": "sample",
+            "date": datetime.strptime(arr[0], DATE_TEMPLATE),
+            "http_code": arr[1],
+            "time_namelookup": arr[2],
+            "time_connect": arr[3],
+            "time_appconnect": arr[4],
+            "time_pretransfer": arr[5],
+            "time_redirect": arr[6],
+            "time_starttransfer": arr[7],
+            "time_total": arr[8],
+            "num_redirects": arr[9],
+            "url": str(arr[10])
+        }
+
+    except Exception as error:
+        LOGGER.warning('malformed sample line: {0}'.format(arr.join(' ')))
+        LOGGER.error(error)
+        return None
 
 def format_error(arr):
     """format arr into failed curl"""
 
-    return {
-        "type": "error",
-        "date": datetime.strptime(arr[0], DATE_TEMPLATE),
-        "exit_code": arr[1],
-        "url": arr[2]
-    }
+    try:
+        return {
+            "type": "error",
+            "date": datetime.strptime(arr[0], DATE_TEMPLATE),
+            "exit_code": arr[1],
+            "url": arr[2],
+            "user": arr[3] if len(arr) > 3 else ""
+        }
+
+    except Exception as error:
+        LOGGER.warning('malformed error line: {0}'.format(arr.join(' ')))
+        LOGGER.error(error)
+        return None
 
 def parse_line(info):
     """transform the given line into api object"""
@@ -70,50 +81,59 @@ def parse_line(info):
     if not len(info):
         return None
 
+    chunks = info.split(' ')
+    size = len(chunks)
+
+    if size == 11:
+        return format_sample(chunks)
+
+    elif size == 3 or size == 4:
+        return format_error(chunks)
+
     else:
-        try:
-            chunks = info.split(' ')
-            type = len(chunks)
-
-            if type == 11:
-                return format_sample(chunks)
-
-            elif type == 3 or type == 4:
-                return format_error(chunks)
-
-            else:
-                LOGGER.warning('got malformed line {0}'.format(info))
-                return None
-
-        except Exception as error:
-            LOGGER.error('Error parsing chunk: {0}. error:{1}'.format(info, error))
-            return None
+        LOGGER.warning('unexpected line format. size:{0} line:{1}'.format(size, info))
+        return None
 
 class PingPong(object):
 
         @falcon.before(sanitize_data)
         def on_get(self, req, resp, start_date, host):
+
             """Handles GET requests"""
-            LOGGER.info('querying {0} for host:{1}'.format(str(start_date), host))
+            LOGGER.info('querying back {0} for host:{1}'.format(str(start_date), host))
 
             data = []
-            now = datetime.now() - start_date
+            min_date = datetime.now() - start_date
+
             try:
                 with open('/var/DB', 'r') as db:
-                    for line in db:
+
+                    for line in reversed(db.readlines()):
+                        line = re.sub("\n$", "", line)
                         chunk = parse_line(line)
-                        if chunk and chunk["date"] and chunk["date"] > now:
+
+                        if not chunk:
+                            continue
+
+                        if chunk["date"] < min_date:
+                            LOGGER.info('stop searching after when hit {0}'.format(line))
+                            break
+
+                        if chunk["url"] == host or len(host) == 0:
                             chunk["date"] = unicode(chunk["date"])
                             data.append(chunk)
 
             except Exception as error:
                 LOGGER.error(error)
-                raise falcon.HTTPError(falcon.HTTP_400, 'cant parse DB')
+                raise falcon.HTTPError(falcon.HTTP_400, 'invalid DB')
 
+            LOGGER.info('got {0} results.'.format(len(data)))
             resp.body = json.dumps(data)
 
 """ webserver init """
-api = falcon.API()
 handler = PingPong()
-api.add_route('/', handler)
-api.add_route('/{start_date}', handler)
+
+api = falcon.API()
+api.add_route('/api/', handler)
+api.add_route('/api/{start_date}', handler)
+
